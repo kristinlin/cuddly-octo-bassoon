c960b8a26552924f9d903be421dbfdabc5758f29
kristinlin <klin11@stuy.edu>
2018-04-30 14:05:38 -0400

    cw

---
+++ reductio.py
@@ -0,0 +1,14 @@
+
+def single_word(word, book):
+    print len([ x for x in book if x == word])
+#    print reduce(lambda a, b : a+1 if word == b, book)
+
+
+def phrase(p, book):
+    pass
+#    print len([ x for x in book if x == word])
+
+def most(book):
+    
+    
+single_word("hello", ["hello", "world", "hello", "Hello"])
