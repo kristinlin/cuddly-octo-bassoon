232b7dcac0ce01286967fd5ea8ab834da5fa7132
Kristin Lin <klin11@stuy.edu>
2018-05-01 00:03:07 -0400

    halfway

--- reductio.py
+++ reductio.py
@@ -1,14 +1,22 @@
+import string
+
+#get book file
+file = "1400.txt"
+book = open(file, 'r')
+
+#remove all punctuation and newlines
+text = ''.join([l for l in book.read() if l not in string.punctuation and l!='\n' and l!='\r'])
+
+#get all words in book
+words = [w for w in text.split(' ') if w!='']
 
 def single_word(word, book):
-    print len([ x for x in book if x == word])
-#    print reduce(lambda a, b : a+1 if word == b, book)
-
+    return len([ x for x in book if x == word])
 
 def phrase(p, book):
-    pass
-#    print len([ x for x in book if x == word])
+    return reduce((lambda a, b: a+b), [single_word(a, book) for a in p.split(' ')])
 
 def most(book):
-    
-    
-single_word("hello", ["hello", "world", "hello", "Hello"])
+    pass
+
+print phrase("hello world", ["hello", "world", "hello", "Hello"])
